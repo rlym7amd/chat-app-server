747d3772e303632a268110b8869570327c7f131a
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2025-01-25 23:16:58 +0100

    use local db for developement

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -6,7 +6,11 @@ import { log } from "../logger";
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT!),
 });
 
 pool.on("error", (err) => {
