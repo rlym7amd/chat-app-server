a7017bf4db17008cc0f415a80d2fe956b7e06c43
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2025-01-26 12:27:12 +0100

    update drizzle config to use local db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,6 +6,10 @@ export default defineConfig({
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    user: process.env.DB_USER!,
+    host: process.env.DB_HOST!,
+    database: process.env.DB_NAME!,
+    password: process.env.DB_PASSWORD!,
+    port: parseInt(process.env.DB_PORT!),
   },
 });
