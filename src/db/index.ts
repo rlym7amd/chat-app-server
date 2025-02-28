c1915e1accef2124ae43b440c5d8d9e22fd01295
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2025-02-28 15:47:08 +0000

    refactor: remove destructuring all props from proccess.env

--- src/db/index.ts
+++ src/db/index.ts
@@ -6,14 +6,12 @@ import { log } from "../logger";
 
 dotenv.config();
 
-const { DB_USER, DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT } = process.env;
-
 const pool = new Pool({
-  user: DB_USER!,
-  host: DB_HOST!,
-  database: DB_NAME!,
-  password: DB_PASSWORD!,
-  port: parseInt(DB_PORT!),
+  user: process.env.DB_USER!,
+  host: process.env.DB_HOST!,
+  database: process.env.DB_NAME!,
+  password: process.env.DB_PASSWORD!,
+  port: parseInt(process.env.DB_PORT!),
 });
 
 pool.on("error", (err) => {
