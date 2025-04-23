54858240464a93ea7a897117d827d4480cffc33c
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2025-04-23 05:06:46 +0100

    fix: finally running db migrations on production

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,22 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
+const isProduction = process.env.NODE_ENV!;
+
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
-  dbCredentials: {
-    user: process.env.DB_USER!,
-    host: process.env.DB_HOST!,
-    database: process.env.DB_NAME!,
-    password: process.env.DB_PASSWORD!,
-    port: parseInt(process.env.DB_PORT!),
-    ssl: false,
-  },
+  dbCredentials: isProduction
+    ? {
+        url: process.env.DATABASE_URL!,
+      }
+    : {
+        user: process.env.DB_USER!,
+        host: process.env.DB_HOST!,
+        database: process.env.DB_NAME!,
+        password: process.env.DB_PASSWORD!,
+        port: parseInt(process.env.DB_PORT!),
+        ssl: false,
+      },
 });
